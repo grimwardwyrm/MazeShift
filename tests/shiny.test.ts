87876a963fdffa756302e6cf01034ff74d695b9b
grimwardwyrm <radardish33@gmail.com>
2026-09-10 18:59:41 -0600

    Test ideas

---
+++ tests/shiny.test.ts
@@ -0,0 +1,19 @@
+// Default constructor initializes default point
+// Default constructor initializes isConsumed
+// Default constructor initializes sprite
+// Default constructor initializes power
+
+// Constructor initializes point
+// Constructor initializes isConsumed
+// Constructor initializes sprite
+// Constructor initializes power
+
+// BetterShiny constructor initializes point
+// BetterShiny constructor initializes isConsumed
+// BetterShiny constructor initializes sprite
+// BetterShiny constructor initializes power
+
+// SuperShiny constructor initializes point
+// SuperShiny constructor initializes isConsumed
+// SuperShiny constructor initializes sprite
+// SuperShiny constructor initializes power
