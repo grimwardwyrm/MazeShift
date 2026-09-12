cd65f21d19c222b552022fd6fdae0f8edf47bbe6
grimwardwyrm <radardish33@gmail.com>
2026-09-11 20:33:38 -0600

    Added tests for the point class.

--- tests/point.test.ts
+++ tests/point.test.ts
@@ -1,5 +1,27 @@
-// Default constructor initalizes default coords
+import '../src/point'
 
-// Constructor initalizes coords
+test('Default constructor initializes points to (0,0).', () => {
+    let point: Point = new Point();
+    expect(point.X).toBe(0);
+    expect(point.Y).toBe(0);
+});
 
-// Add adds correctly
+test('Constructor initializes points to (1,2).', () => {
+    let point: Point = new Point(1,2);
+    expect(point.X).toBe(1);
+    expect(point.Y).toBe(2);
+});
+
+test('Add correctly adds points (1,2) and (3,4) to be (4,6).', () => {
+    let pointA: Point = new Point(1,2);
+    let pointB: Point = new Point(3,4);
+    let expected: Point = new Point(4,6);
+    expect(pointA.Add(pointB)).toEqual(expected);
+});
+
+test('Add correctly adds points (1,2) and (-3,-4) to be (-2,-2).', () => {
+    let pointA: Point = new Point(1,2);
+    let pointB: Point = new Point(-3,-4);
+    let expected: Point = new Point(-2,-2);
+    expect(pointA.Add(pointB)).toEqual(expected);
+});
